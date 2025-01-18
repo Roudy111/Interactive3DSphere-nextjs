d2de4372be3b0012bbd09d024871c2566ab87f27
Siamak Roudyani <142318187+Roudy111@users.noreply.github.com>
2025-01-18 16:12:13 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-'use client';  // Add this line at the top
-
-import dynamic from 'next/dynamic'
-
-const NoiseVertex3D = dynamic(() => import('../components/NoiseVertex3D'), {
-  ssr: false,
-  loading: () => <div>Loading...</div>
-})
+import ClientPage from '../components/ClientPage'
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-900">
-      <NoiseVertex3D />
-    </div>
-  )
+  return <ClientPage />
 }
