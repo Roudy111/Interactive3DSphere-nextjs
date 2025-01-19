48c9dd1bd59646abe89fe0558b8e8c1a96c2160c
Siamak Roudyani <142318187+Roudy111@users.noreply.github.com>
2025-01-19 01:09:29 +0100

    Update NoiseVertex3DWrapper.tsx

--- components/NoiseVertex3DWrapper.tsx
+++ components/NoiseVertex3DWrapper.tsx
@@ -1,15 +1,10 @@
 "use client";
-
 import dynamic from 'next/dynamic'
 
-const NoiseVertex3D = dynamic(() => import('./NoiseVertex3D/index'), {
+const NoiseVertex3D = dynamic(() => import('./NoiseVertex3D'), {
   ssr: false,
 })
 
 export default function NoiseVertex3DWrapper() {
-  return (
-    <div className="fixed inset-0 w-screen h-screen">
-      <NoiseVertex3D />
-    </div>
-  )
+  return <NoiseVertex3D />
 }
